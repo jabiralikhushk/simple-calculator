6664f18e28f9e69a555bb7a2ac5aadc11e314948
Jabir ali <163226601+jabiralikhushk@users.noreply.github.com>
2024-03-25 07:54:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+var answer = await inquirer_1.default.prompt([
+    { message: "price of first item of book", type: "number", name: "firstItem" },
+    { message: "price of second item of book", type: "number", name: "secondItem" },
+    { message: "price of third item of book", type: "number", name: "thirdItem" },
+    {
+        message: "select one of the following operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    }
+]);
+// conditional statement:
+if (answer.operators === "addition") {
+    console.log(answer.firstItem + answer.secondItem + answer.thirdItem);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstItem - answer.secondItem + answer.thirdItem);
+}
+else if (answer.operator === "multipliction") {
+    console.log(answer.firstItem * answer.secondItem + answer.thirdItem);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstItem / answer.secondItem + answer.thirdItem);
+}
+else {
+    console.log("please select a valid operator");
+}
